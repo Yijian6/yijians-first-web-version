a756cc3a8be0ad6d8d2be14ecf46659b81cb8375
lyijian86-source <lyijian86@gmail.com>
2026-07-21 15:14:02 +0800

    content: update Minecraft world (1 article(s))

--- mc/world-data.js
+++ mc/world-data.js
@@ -25,6 +25,21 @@ window.MC_WORLD = {
       ],
       "lastDate": "2026-07-21",
       "stale": false
+    },
+    {
+      "name": "计算机系统基础2",
+      "nameEn": "CSAPP",
+      "slug": "csapp",
+      "floors": [
+        {
+          "n": 1,
+          "title": "我们必须承认，这是一个草稿，不是一片正文",
+          "date": "2026-07-21"
+        }
+      ],
+      "ghosts": [],
+      "lastDate": "2026-07-21",
+      "stale": false
     }
   ],
   "log": [
@@ -33,6 +48,12 @@ window.MC_WORLD = {
       "title": "链表与数组之争",
       "domainName": "数据结构",
       "url": "mc/data-structures/链表与数组之争.html"
+    },
+    {
+      "date": "2026-07-21",
+      "title": "我们必须承认，这是一个草稿，不是一片正文",
+      "domainName": "计算机系统基础2",
+      "url": "mc/csapp/这是一个草稿哈哈.html"
     },
     {
       "date": "2026-07-15",
